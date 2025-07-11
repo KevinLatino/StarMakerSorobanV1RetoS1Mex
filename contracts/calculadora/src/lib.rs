5829c66667acc8d6c9cd08a09e8cae5dc4b94e5e
KevinLatino <latinokevin9@gmail.com>
2025-07-10 23:01:27 -0600

    adding stored_result and sum function

--- contracts/calculadora/src/lib.rs
+++ contracts/calculadora/src/lib.rs
@@ -1,23 +1,24 @@
 #![no_std]
 
+use soroban_sdk::{contract, contractimpl, Env, symbol_short, Symbol};
 
-use soroban_sdk::{contract, contractimpl,  Env, symbol_short, Symbol};
-const RESULTADO: Symbol = symbol_short!("RESULTADO");
+const RESULT: Symbol = symbol_short!("RESULT");
+
 #[contract]
 pub struct Contract;
-
 
 #[contractimpl]
 impl Contract {
     
-    pub fn sumar(env: Env, a:i128, b:i128) -> i128 {
-      //Implementar función que sume dos números
-      return 30;
+    pub fn add(env: Env, a: i128, b: i128) -> i128 {
+        let sum = a + b;
+        env.storage().instance().set(&RESULT, &sum);
+        return sum;
     }
 
-    pub fn resultado_anterior(env: Env) -> i128 {
-           //Implementar función que retorne el valor anterior
-            return 30;
+    pub fn previous_result(env: Env) -> i128 {
+        let stored_result = env.storage().instance().get(&RESULT).unwrap_or(0);
+        return stored_result;
     }
 }
 
